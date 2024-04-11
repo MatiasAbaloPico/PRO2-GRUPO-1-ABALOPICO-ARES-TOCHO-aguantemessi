bd93602097b4c7c3ed349aceeadcc9764edc16a5
AgussAress <aares@udesa.edu.ar>
2024-04-11 20:36:20 -0300

    creamos db, falta completar productos

---
+++ db/datos.js
@@ -0,0 +1,11 @@
+datos = {
+    usuario: {
+        email: "messi@gmail.com",
+        usuario: "messi",
+        contrasenia: "******",
+        fechaNacimiento: "12/12/1983",
+        dni: "37820176",
+        fotoPerfil: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf0LuMnGHfTMV19hjWkwWApf2Whm2ulDBL8uJtq7CBcg&s"
+    },
+    productos: [{}],
+}
