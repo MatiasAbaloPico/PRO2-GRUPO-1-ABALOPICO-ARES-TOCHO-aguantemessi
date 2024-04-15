e8a3a0146cc85d675585396ca77e13002309f51e
MatiasAbaloPico <mabalopico@udesa.edu.ar>
2024-04-14 23:35:27 -0300

    correcion de error en db

--- db/datos.js
+++ db/datos.js
@@ -7,212 +7,209 @@ datos = {
         dni: "37820176",
         fotoPerfil: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf0LuMnGHfTMV19hjWkwWApf2Whm2ulDBL8uJtq7CBcg&s"
     },
-    productos: [{}],
+    productos: [{
+            imagen: "https://www.bobswatches.com/rolex-blog/wp-content/uploads/2021/07/Screen-Shot-2021-07-02-at-4.21.12-PM.jpg",
+            producto: "Date Just",
+            descripcion: "Rolex Datejust (Nuevo): ¡Elegancia y precisión combinadas! Este Rolex Datejust es un nuevo cronómetro certificado que añadirá un toque de distinción a tu estilo.",
+        },
+        {
+            imagen: "https://www.swisswatchexpo.com/TheWatchClub/wp-content/uploads/2023/12/What-Rolex-Submariner-Is-the-Best-Investment-Rolex-Submariner-Yellow-Gold-Black-Dial-Bezel-Mens-Watch-126618.jpg",
+            producto: "Submariner",
+            descripcion: "Rolex Submariner (Usado): Sumérgete en la leyenda con este Rolex Submariner. Usado pero en excelente estado, te ofrece la resistencia al agua que necesitas hasta 300 metros de profundidad.",
+        },
+        {
+            imagen: "https://www.bobswatches.com/rolex-blog/wp-content/uploads/2022/07/Rolex-GMT-Master-II-126720vtnr.png",
+            producto: "GMT-MasterII",
+            descripcion: "Rolex GMT Master II (Nuevo): ¡Viaja con estilo y funcionalidad! Este GMT Master II es nuevo, con la capacidad de rastrear dos husos horarios simultáneamente, perfecto para tus aventuras globales.",
+        },
+        {
+            imagen: "https://assets-global.website-files.com/636a2d3c26aa845d12b17679/645a43698b56d1226d07ba7f_Petite-Geneve-Petrovic-2019-11-18-rolex-day-date-40-ref-228206-7.webp",
+            producto: "Day-Date",
+            descripcion: "Rolex Day-Date (Usado): Haz una declaración de lujo con este Rolex Day-Date. Aunque es usado, su prestigio y elegancia son innegables, con la ventana de día y fecha para mantenerte siempre al tanto.",
+        },
+        {
+            imagen: "https://hodinkee.imgix.net/uploads/article/hero_image/352/RolexDaytonaPlatinum_6.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12",
+            producto: "Cosmograph Daytona",
+            descripcion: "Rolex Cosmograph Daytona (Nuevo): ¡Corre hacia la grandeza con este Cosmograph Daytona! Nuevo y listo para impresionar, su diseño deportivo y su cronógrafo de precisión te llevarán a la victoria.",
+        },
+        {
+            imagen: "https://watchtime.mx/wp-content/uploads/2022/04/Rolex-Oyster-Perpetual-Day-Date-1.jpg",
+            producto: "Oyster Perpetual",
+            descripcion: "Rolex Oyster Perpetual (Nuevo): No hay nada más atemporal que un Oyster Perpetual. Nuevo y listo para enfrentar el mundo, con su resistencia al agua y a los golpes, este Rolex es el compañero perfecto para cada ocasión.",
+        },
+        {
+            imagen: "https://www.bobswatches.com/rolex-blog/wp-content/uploads/2020/08/Screen-Shot-2021-08-23-at-2.03.29-PM.jpg",
+            producto: "Yacht-Master",
+            descripcion: "Rolex Yacht-Master (Usado): Navega con estilo con este Rolex Yacht-Master. Aunque es usado, su inspiración en la navegación de lujo y su estilo marinero te llevarán a nuevas alturas, o más bien, profundidades.",
+        },
+        {
+            imagen: "https://www.bobswatches.com/rolex-blog/wp-content/uploads/2020/08/Rolex_seadweller_126600-7297-Edit.jpg",
+            producto: "Sea-Dweller",
+            descripcion: "Rolex Sea-Dweller (Nuevo): ¡Explora las profundidades con confianza! Este Rolex Sea-Dweller es nuevo y está listo para sumergirse hasta 1,220 metros sin perder un segundo de precisión.",
+        },
+        {
+            imagen: "https://monochrome-watches.com/wp-content/uploads/2018/03/Rolex-Deepsea-126660-Baselworld-2018-7.jpg",
+            producto: "Deepsea",
+            descripcion: "Rolex Deepsea (Usado): Aventúrate más allá con este Rolex Deepsea. Aunque es usado, su resistencia extrema al agua hasta 3,900 metros te llevará a nuevos descubrimientos en las profundidades del océano.",
+        },
+        {
+            imagen: "https://i.ebayimg.com/images/g/hrAAAOSwQ8tj6pVD/s-l1600.jpg",
+            producto: "Air-King",
+            descripcion: "Rolex Air-King (Nuevo): ¡Despega hacia nuevas alturas con este Rolex Air-King! Nuevo y listo para conquistar el cielo, su homenaje a la aviación y su resistencia lo convierten en un compañero confiable para cada vuelo.",
+        },
+        comentarios = {
+            comentario_1: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_2: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_3: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_4: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_5: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_6: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_7: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_8: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_9: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_10: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_11: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_12: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_13: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_14: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_15: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_16: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_17: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_18: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_19: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_20: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_21: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_22: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_23: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_24: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_25: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_26: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_27: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_28: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_29: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+            comentario_30: {
+                nom_usu: "",
+                text_com: "",
+                img_pp: "",
+            },
+        },
+    ],
 };
 
 module.exports = datos;
-
-productos = {
-    dateJust: {
-        imagen:"https://www.bobswatches.com/rolex-blog/wp-content/uploads/2021/07/Screen-Shot-2021-07-02-at-4.21.12-PM.jpg",
-        producto: "Date Just",
-        descripcion: "Rolex Datejust (Nuevo): ¡Elegancia y precisión combinadas! Este Rolex Datejust es un nuevo cronómetro certificado que añadirá un toque de distinción a tu estilo.",
-    },
-    submariner: {
-        imagen:"https://www.swisswatchexpo.com/TheWatchClub/wp-content/uploads/2023/12/What-Rolex-Submariner-Is-the-Best-Investment-Rolex-Submariner-Yellow-Gold-Black-Dial-Bezel-Mens-Watch-126618.jpg",
-        producto: "Submariner",
-        descripcion: "Rolex Submariner (Usado): Sumérgete en la leyenda con este Rolex Submariner. Usado pero en excelente estado, te ofrece la resistencia al agua que necesitas hasta 300 metros de profundidad.",
-    },
-    gmt_masterII: {
-        imagen:"https://www.bobswatches.com/rolex-blog/wp-content/uploads/2022/07/Rolex-GMT-Master-II-126720vtnr.png",
-        producto: "GMT-MasterII",
-        descripcion: "Rolex GMT Master II (Nuevo): ¡Viaja con estilo y funcionalidad! Este GMT Master II es nuevo, con la capacidad de rastrear dos husos horarios simultáneamente, perfecto para tus aventuras globales.",
-    },
-    day_date: {
-        imagen:"https://assets-global.website-files.com/636a2d3c26aa845d12b17679/645a43698b56d1226d07ba7f_Petite-Geneve-Petrovic-2019-11-18-rolex-day-date-40-ref-228206-7.webp",
-        producto: "Day-Date",
-        descripcion: "Rolex Day-Date (Usado): Haz una declaración de lujo con este Rolex Day-Date. Aunque es usado, su prestigio y elegancia son innegables, con la ventana de día y fecha para mantenerte siempre al tanto.",
-    },
-    cosmographDaytona: {
-        imagen:"https://hodinkee.imgix.net/uploads/article/hero_image/352/RolexDaytonaPlatinum_6.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12",
-        producto: "Cosmograph Daytona",
-        descripcion: "Rolex Cosmograph Daytona (Nuevo): ¡Corre hacia la grandeza con este Cosmograph Daytona! Nuevo y listo para impresionar, su diseño deportivo y su cronógrafo de precisión te llevarán a la victoria.",
-    },
-    oysterPerpetual: {
-        imagen:"https://watchtime.mx/wp-content/uploads/2022/04/Rolex-Oyster-Perpetual-Day-Date-1.jpg",
-        producto: "Oyster Perpetual",
-        descripcion: "Rolex Oyster Perpetual (Nuevo): No hay nada más atemporal que un Oyster Perpetual. Nuevo y listo para enfrentar el mundo, con su resistencia al agua y a los golpes, este Rolex es el compañero perfecto para cada ocasión.",
-    },
-    yacht_master: {
-        imagen:"https://www.bobswatches.com/rolex-blog/wp-content/uploads/2020/08/Screen-Shot-2021-08-23-at-2.03.29-PM.jpg",
-        producto: "Yacht-Master",
-        descripcion: "Rolex Yacht-Master (Usado): Navega con estilo con este Rolex Yacht-Master. Aunque es usado, su inspiración en la navegación de lujo y su estilo marinero te llevarán a nuevas alturas, o más bien, profundidades.",
-    },
-    sea_dweller: {
-        imagen:"https://www.bobswatches.com/rolex-blog/wp-content/uploads/2020/08/Rolex_seadweller_126600-7297-Edit.jpg",
-        producto: "Sea-Dweller",
-        descripcion: "Rolex Sea-Dweller (Nuevo): ¡Explora las profundidades con confianza! Este Rolex Sea-Dweller es nuevo y está listo para sumergirse hasta 1,220 metros sin perder un segundo de precisión.",
-    },
-    deepsea: {
-        imagen:"https://monochrome-watches.com/wp-content/uploads/2018/03/Rolex-Deepsea-126660-Baselworld-2018-7.jpg",
-        producto: "Deepsea",
-        descripcion: "Rolex Deepsea (Usado): Aventúrate más allá con este Rolex Deepsea. Aunque es usado, su resistencia extrema al agua hasta 3,900 metros te llevará a nuevos descubrimientos en las profundidades del océano.",
-    },
-    air_king: {
-        imagen:"https://i.ebayimg.com/images/g/hrAAAOSwQ8tj6pVD/s-l1600.jpg",
-        producto: "Air-King",
-        descripcion: "Rolex Air-King (Nuevo): ¡Despega hacia nuevas alturas con este Rolex Air-King! Nuevo y listo para conquistar el cielo, su homenaje a la aviación y su resistencia lo convierten en un compañero confiable para cada vuelo.",
-    },
-    comentarios: {
-        comentario_1:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_2:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_3:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_4:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_5:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_6:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_7:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_8:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_9:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_10:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_11:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",  
-        },
-        comentario_12:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_13:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_14:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_15:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_16:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_17:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_18:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_19:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_20:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_21:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_22:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_23:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_24:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_25:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_26:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_27:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"", 
-        },
-        comentario_28:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_29:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",
-        },
-        comentario_30:{
-            nom_usu:"",
-            text_com:"",
-            img_pp:"",  
-        },
-    },
-}
