648bf786b4d2aed44db3330ab51b47af5c486646
AgussAress <aares@udesa.edu.ar>
2024-05-23 15:25:30 -0300

    comentamos session, lo dejamos para mas adelante

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
+/*app.use(session({
   secrets: "mysecret",
   resave: false,
   saveUninitialized: true,
-}))
+}))*/
 
 
 app.use('/', indexRouter);
