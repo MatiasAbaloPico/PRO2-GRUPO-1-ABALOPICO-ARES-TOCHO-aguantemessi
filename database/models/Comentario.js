b8348a305a102a3cc7c53717397e64e30d6c9336
AgussAress <aares@udesa.edu.ar>
2024-06-06 09:49:51 -0300

    mi parte del modelo - Comentario.js

---
+++ database/models/Comentario.js
@@ -0,0 +1,43 @@
+module.exports = function(sequelize, dataTypes){
+    /* Alias */
+    let alias = "Comentario";
+
+    /* Columnas */
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        idPost: {
+            type: dataTypes.INTEGER
+        },
+        idUsuario: {
+            type: dataTypes.INTEGER
+        },
+        textoComentario: {
+            type: dataTypes.STRING
+        },
+        createdAt: {
+            type: dataTypes.TIME
+        },
+        updatedAt: {
+            type: dataTypes.TIME
+        },
+        deletedAt: {
+            type: dataTypes.TIME
+        }
+    }
+
+    /* Tablas */
+    let config = {
+        tableName: "comentarios",
+        timestamps: true,
+        underscored: false
+    }
+
+    /* define Modelo */
+    let Comentario = sequelize.define(alias, cols, config)
+
+    return Comentario;
+};
