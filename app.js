8011c0c49a38130b2327afc4de77850764c0c5dd
AgussAress <aares@udesa.edu.ar>
2024-06-13 15:30:07 -0300

    session en app (mini cambio)

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use(session({
-  secrets: "mysecret",
+app.use(session({
+  secret: "mysecret",
   resave: false,
   saveUninitialized: true,
-}))*/
+}))
 
 
 app.use('/', indexRouter);
