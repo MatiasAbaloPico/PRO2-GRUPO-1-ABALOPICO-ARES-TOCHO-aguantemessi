92438968a612edb8ae2186ebfe8fae536e25390c
AgussAress <aares@udesa.edu.ar>
2024-06-17 11:45:22 -0300

    mails duplicados y contraseñas menores a 4 caracteres

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,23 @@
 var express = require('express');
 var router = express.Router();
 const indexController = require("../controller/indexController");
+const db = require("../database/models");
 const { body } = require("express-validator");
+
 const validaciones = [
-    body("mail").isEmail().withMessage("El email ingresado no es válido, vuelve a intentar."),
+    body("mail").notEmpty().isEmail()
+    .withMessage("El email ingresado no es válido, vuelve a intentar.")
+    .custom(function (value) {
+        return db.Usuario.findOne({
+            where: { mail: value },
+        })
+            .then(function (user) {
+                if (user) {
+                    throw new Error("El mail ingresado ya existe, prueba con otro distinto.")
+                }})}),
     body("usuario").notEmpty().withMessage("Debes completar el campo usuario"),
-    body("contrasenia").notEmpty().withMessage("Debes completar el campo contraseña"),
+    body("contrasenia").notEmpty().withMessage("Debes completar el campo contraseña")
+    .isLength({ min: 4 }).withMessage("La contraseña debe tener al menos 4 caracteres"),
 ]
 
 /* GET home page. */
