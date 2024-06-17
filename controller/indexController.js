964d0f97fa634f75c89c33c55a9a58bf6245f3b5
AgussAress <aares@udesa.edu.ar>
2024-06-17 11:07:39 -0300

    express validator

--- controller/indexController.js
+++ controller/indexController.js
@@ -20,37 +20,41 @@ const indexController = {
     res.render("login", { datos: datos });
   },
   almacenar: function(req, res){
-    let errores = validationResult(req);
-    if (errores.isEmpty()) {
+
+    let errors = validationResult(req);
+    
+    if (errors.isEmpty()) {
+      
+      let form = req.body; /* <----- acá guardamos la información del formulario */
+      req.session.datosForm = form
+
+      let registracion = {
+        mail: form.mail,
+        usuario: form.usuario,
+        nombre: form.nombre,
+        apellido: form.apellido,
+        contrasenia: bcryptjs.hashSync(form.contrasenia, 10), /* <---- acá voy a tener que hacer hash */
+        fechaNacimiento: form.fechaNacimiento,
+        dni: form.dni,
+        foto: form.foto,
+      }
+      
+      db.Usuario.create(registracion)
+      .then(function (result) {
+        return res.redirect("/")
+      })
+      .catch(function (err) {
+          return console.log(err);
+      })
 
     } else {
-      
-    }
-    let form = req.body; /* <----- acá guardamos la información del formulario */
-    req.session.datosForm = form
-
-    let registracion = {
-      mail: form.mail,
-      usuario: form.usuario,
-      nombre: form.nombre,
-      apellido: form.apellido,
-      contrasenia: bcryptjs.hashSync(form.contrasenia, 10), /* <---- acá voy a tener que hacer hash */
-      fechaNacimiento: form.fechaNacimiento,
-      dni: form.dni,
-      foto: form.foto,
+      res.render("register", { 
+        errors: errors.array(),
+        old: req.body 
+      });
     }
     
-    
-    
-    db.Usuario.create(registracion)
-    .then(function (result) {
-      return res.redirect("/")
-    })
-    .catch(function (err) {
-        return console.log(err);
-      })
       
-    
   },
 }
 
