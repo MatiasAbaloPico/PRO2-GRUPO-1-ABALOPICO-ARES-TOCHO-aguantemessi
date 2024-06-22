4fe6e56d11ee0c289ba92e216f065d02002cd5fc
MatiasAbaloPico <mabalopico@udesa.edu.ar>
2024-06-22 00:34:11 -0300

    Validaciones login y detalles

--- routes/product.js
+++ routes/product.js
@@ -4,19 +4,19 @@ const productController = require("../controller/productController");
 
 const { body } = require("express-validator");
 
-const validaciones = [
+const validacionesP = [
     body("nombre").notEmpty().withMessage("Debes completar el campo nombre"),
     body("descripcion").notEmpty().withMessage("Debes completar el campo descripcion"),
 ]
 
 /* GET home page. */
 router.get('/product/:id/', productController.product);
-router.get('/search-results', productController.searchResults);
+router.get('/search-results/', productController.searchResults);
 router.get('/product-edit/:id/', productController.productEdit);
-router.get('/product-add', productController.productAdd);
+router.get('/product-add/', productController.productAdd);
 
 /* POST */
-router.post('/product-save', validaciones, productController.productSave);
+router.post('/product-save/', validacionesP, productController.productSave);
 
 
 /* DELETE */
