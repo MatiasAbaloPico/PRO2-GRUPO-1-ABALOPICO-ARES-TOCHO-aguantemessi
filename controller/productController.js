26cd23165e6a2040d3e42f4fd618a556713f16b8
AgussAress <aares@udesa.edu.ar>
2024-06-23 18:07:56 -0300

    ahora se agrega a la base de datos, pero rompe

--- controller/productController.js
+++ controller/productController.js
@@ -65,7 +65,7 @@ const productController = {
         }
         db.Comentario.create(comentario)
         .then ((result) => {
-          return res.render("product")
+          return res.redirect("product/${idProducto}")
         }).catch((err) => {
           console.log(err)
         })
